b45af80678268e8192ce14065e2a4cef648bac3d
bumjun2 <jbj3713@naver.com>
2023-10-04 16:12:22 +0900

    Navigation라이브러리로 하단부 만들기

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
+// App.js
 import React, {useEffect} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Screen1 from './screens/Screen1';
+import Screen2 from './screens/Screen2';
+import Screen3 from './screens/Screen3';
+import Screen4 from './screens/Screen4';
 import SplashScreen from 'react-native-splash-screen';
-import Head from './src/head/Head';
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
 
-  return <Head />;
+  return (
+    //바닥
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Screen1} />
+        <Tab.Screen name="Screen2" component={Screen2} />
+        <Tab.Screen name="Screen3" component={Screen3} />
+        <Tab.Screen name="Screen4" component={Screen4} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
--- src/head/HeadCss.js
+++
@@ -1,9 +0,0 @@
-const {StyleSheet} = require('react-native');
-
-const styels = StyleSheet.create({
-  red: {
-    color: 'blue',
-  },
-});
-
-export default styels;
